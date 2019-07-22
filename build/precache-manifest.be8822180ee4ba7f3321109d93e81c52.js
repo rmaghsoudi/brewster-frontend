b9ca2fadc81ea7051111012c527838f7ecb45aaa
rmaghsoudi <romy_mag@outlook.com>
2019-07-22 16:02:30 -0400

    build init

---
+++ build/precache-manifest.be8822180ee4ba7f3321109d93e81c52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab1cdb1a6d79d2a94d71fd58b3e4053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdb32fda8b35baa12cfa",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d8faa27dab47492f8746",
+    "url": "/static/css/main.a1f85211.chunk.css"
+  },
+  {
+    "revision": "cdb32fda8b35baa12cfa",
+    "url": "/static/js/2.e6add21e.chunk.js"
+  },
+  {
+    "revision": "d8faa27dab47492f8746",
+    "url": "/static/js/main.39dbe992.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c90f026c22d3b66cb31ddd82511a879",
+    "url": "/static/media/beer-wall1.2c90f026.jpg"
+  },
+  {
+    "revision": "d1284b856b76264c3de6e27594e92b6e",
+    "url": "/static/media/brewster.d1284b85.png"
+  },
+  {
+    "revision": "a9248d1d000d66c872c939e0eef481d7",
+    "url": "/static/media/brewster_banner.a9248d1d.png"
+  },
+  {
+    "revision": "d5f51047d8bd6327ec4a74361a7aae7f",
+    "url": "/static/media/default_avatar.d5f51047.jpg"
+  },
+  {
+    "revision": "c4a12b105b3a001868e226d321b8c104",
+    "url": "/static/media/shelf3.c4a12b10.png"
+  }
+]);
